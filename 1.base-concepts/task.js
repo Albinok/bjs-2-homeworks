000bbd16f290ec28a3b0ef2c826dc80e84219327
Albina <alkisa17@yandex.ru>
2022-11-06 10:19:34 +0500

    cod

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,16 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
+  let d = b**2-4*a*c;
+  let x;
+  // let quadraticEquation = a*x**2+b*x+c;
+  if (d < 0) {
+    console.log('корней нет')
+  } else if (d = 0) {
+    x = -b/(2*a);
+    arr.push(x); 
+  }
+
   // код для задачи №1 писать здесь
   return arr; // array
 }
