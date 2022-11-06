731e8dce42df8b7001a0c61f8eef2a089ee4c281
Albina <alkisa17@yandex.ru>
2022-11-06 21:55:35 +0500

    finish

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,24 +1,28 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let d = b**2-4*a*c;
+  let d = b ** 2 - 4 * a * c;
   let x;
+  let x2;
   // let quadraticEquation = a*x**2+b*x+c;
   if (d < 0) {
-    console.log('корней нет')
-  } else if (d = 0) {
-    x = -b/(2*a);
+    return arr;
+  } else if (d === 0) {
+    x = -b / (2 * a);
     arr.push(x); 
+  } else {
+    x = (-b + Math.sqrt(d)) / (2 * a);
+    arr.push(x);
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x2);
   }
-
-  // код для задачи №1 писать здесь
   return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+// function calculateTotalMortgage(percent, contribution, amount, date) {
+//   let totalAmount;
 
-  // код для задачи №2 писать здесь
+//   // код для задачи №2 писать здесь
 
-  return totalAmount;
-}
+//   return totalAmount;
+// }
