1ca608ffd3fa3f23f03e6aa20f7bf348fea102aa
Albina <alkisa17@yandex.ru>
2022-11-08 01:58:47 +0500

    finish2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,27 +2,19 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = b ** 2 - 4 * a * c;
-  let x;
-  let x2;
-  // let quadraticEquation = a*x**2+b*x+c;
-  if (d < 0) {
-    return arr;
-  } else if (d === 0) {
-    x = -b / (2 * a);
-    arr.push(x); 
-  } else {
-    x = (-b + Math.sqrt(d)) / (2 * a);
-    arr.push(x);
-    x2 = (-b - Math.sqrt(d)) / (2 * a);
-    arr.push(x2);
+  if (d === 0) {
+    arr.push(-b / (2 * a));
+  } else if (d > 0) {
+    arr.push(-b + Math.sqrt(d)) / (2 * a);
+    arr.push(-b - Math.sqrt(d)) / (2 * a);
   }
-  return arr; // array
+  return arr; 
 }
 
-// function calculateTotalMortgage(percent, contribution, amount, date) {
-//   let totalAmount;
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  let totalAmount;
 
-//   // код для задачи №2 писать здесь
+  // код для задачи №2 писать здесь
 
-//   return totalAmount;
-// }
+  return totalAmount;
+}
