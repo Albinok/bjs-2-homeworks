1183386bea044c0bf3599648ecbacd9d9b8e44e3
Albina <alkisa17@yandex.ru>
2022-12-16 00:12:04 -0500

    dz

---
+++ 7.async/task.js
@@ -0,0 +1,57 @@
+class AlarmClock {
+  constructor(){
+   this.alarmCollection = [];
+   this.timerId = null;
+  }
+
+  addClock(time, callback, id){
+    if (id === undefined){
+      throw new Error ('id нет');
+    } else if (this.alarmCollection.some(item => item.id === id)){
+      console.error('Звонок с уже существует');
+    } else {
+      return this.alarmCollection.push({time, callback, id})
+    }
+  }
+
+  removeClock(id){
+    let remoteCall = this.alarmCollection.filter(item => item.id !== id);
+    if (remoteCall.length < this.alarmCollection.length){
+      this.alarmCollection = remoteCall;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  getCurrentFormattedTime(){
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+
+    return `${hours} : ${minutes}`;
+  }
+  start() {
+    let checkClock = () => {
+      this.alarmCollection.forEach((item) => {
+        if (item.time === this.getCurrentFormattedTime()){
+          item.callback();
+        }
+      })
+    }
+    if (this.timerId === null){
+      this.timerId = setInterval(checkClock, 1000);
+    }
+  }
+  stop(){
+    if (this.timerId){
+      clearInterval(this.timerId);
+      this.timerId = null;
+    }
+  }
+  printAlarms(){
+    this.alarmCollection.forEach(item => console.log(`id: ${item.id}, time: ${item.time}`));
+  }
+}
+
+
