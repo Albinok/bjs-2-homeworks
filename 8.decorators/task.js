d66308ea2ee9c470bde99e5d1c022999131e3620
Albina <alkisa17@yandex.ru>
2022-12-22 08:59:10 -0500

    8.1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,50 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  const cash = [];
+
+  function wrapper(...args){
+    const hash = args.join(',');
+
+    const objectInCache = cash.find((item) => {
+      return hash === Object.keys(item)[0];
+    })
+
+    const result = func(...args);
+    
+    if (!objectInCache){
+      cash.push({[hash]: result});
+    } else {
+      console.log('Из кэша: ' + Object.values(objectInCache));
+      return 'Из кэша: ' + Object.values(objectInCache);
+    }
+
+    if (cash.length > 5){
+      cash.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  let isThrottle = false;
+
+  function wrapper(...args){
+    wrapper.allCount++;
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func(...args);
+      wrapper.count++;
+    }, delay)
+
+    if (!isThrottle){
+      func(...args);
+      isThrottle = true;
+    }
+  }
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  return wrapper;
 }
